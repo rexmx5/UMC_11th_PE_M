ceeb9a34b4172bbfef67e3c033739b930c98865b
rexmx5 <69381113+rexmx5@users.noreply.github.com>
2026-09-16 00:14:47 +0900

    chore: Flutter 프로젝트를 생성한다

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('Hello MovieLog!'))),
+    ),
+  );
+}
